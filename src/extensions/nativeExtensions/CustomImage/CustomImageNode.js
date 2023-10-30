29a904621d966059d76e04ca7836298c63a64966
Daniel Schwab <schwab@infai.org>
2023-10-30 15:02:29 +0100

    Add clipboard paste listener to handle pasting of images correctly

--- src/extensions/nativeExtensions/CustomImage/CustomImageNode.js
+++ src/extensions/nativeExtensions/CustomImage/CustomImageNode.js
@@ -1,15 +1,5 @@
 import { Node, Plugin } from 'tiptap'
-import { nodeInputRule } from 'tiptap-commands'
 import { filterImages } from './CustomImageHelper'
-/**
- * Matches following attributes in Markdown-typed image: [, alt, src, title]
- *
- * Example:
- * ![Lorem](image.jpg) -> [, "Lorem", "image.jpg"]
- * ![](image.jpg "Ipsum") -> [, "", "image.jpg", "Ipsum"]
- * ![Lorem](image.jpg "Ipsum") -> [, "Lorem", "image.jpg", "Ipsum"]
- */
-const IMAGE_INPUT_REGEX = /!\[(.+|:?)]\((\S+)(?:(?:\s+)["'](\S+)["'])?\)/
 
 export default class CustomImageNode extends Node {
   constructor (options) {
@@ -60,59 +50,49 @@ export default class CustomImageNode extends Node {
     }
   }
 
-  inputRules ({ type }) {
-    return [
-      nodeInputRule(IMAGE_INPUT_REGEX, type, match => {
-        const [, alt, src, title] = match
-        return {
-          src,
-          alt,
-          title
-        }
-      })
-    ]
-  }
-
   get plugins () {
     const fileTypes = this.fileTypes
     const maxFileSize = this.maxFileSize
     const filterErrorFunc = this.filterErrorFunc
+
+    const handleImageEvent = function (view, event, files, coordinates) {
+      if (!files || files.length === 0) {
+        return
+      }
+
+      event.preventDefault()
+
+      const images = filterImages(Array.from(files), fileTypes, maxFileSize, filterErrorFunc)
+
+      if (images.length === 0) {
+        return
+      }
+
+      const { schema } = view.state
+
+      images.forEach(image => {
+        const reader = new FileReader()
+
+        reader.onload = readerEvent => {
+          const node = schema.nodes.custom_image.create({
+            src: readerEvent.target.result
+          })
+          const transaction = coordinates ? view.state.tr.insert(coordinates.pos, node) : view.state.tr.replaceSelectionWith(node)
+          view.dispatch(transaction)
+        }
+        reader.readAsDataURL(image)
+      })
+    }
+
     return [
       new Plugin({
         props: {
           handleDOMEvents: {
             drop (view, event) {
-              const hasFiles = event.dataTransfer &&
-                                event.dataTransfer.files &&
-                                event.dataTransfer.files.length
-
-              if (!hasFiles) {
-                return
-              }
-
-              event.preventDefault()
-
-              const images = filterImages(Array.from(event.dataTransfer.files), fileTypes, maxFileSize, filterErrorFunc)
-
-              if (images.length === 0) {
-                return
-              }
-
-              const { schema } = view.state
-              const coordinates = view.posAtCoords({ left: event.clientX, top: event.clientY })
-
-              images.forEach(image => {
-                const reader = new FileReader()
-
-                reader.onload = readerEvent => {
-                  const node = schema.nodes.custom_image.create({
-                    src: readerEvent.target.result
-                  })
-                  const transaction = view.state.tr.insert(coordinates.pos, node)
-                  view.dispatch(transaction)
-                }
-                reader.readAsDataURL(image)
-              })
+              handleImageEvent(view, event, event.dataTransfer && event.dataTransfer.files, view.posAtCoords({ left: event.clientX, top: event.clientY }))
+            },
+            paste (view, event) {
+              handleImageEvent(view, event, event.clipboardData && event.clipboardData.files, null)
             }
           }
         }
